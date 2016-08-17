b41c9fe95fd6ef63e2b47077266ef3d359714952
zanjs <root@zanjs.com>
2016-08-17 15:14:47 +0800

    undefined

--- a.js
+++ a.js
@@ -1,7 +1,18 @@
 var s = function (){
     var b=1;
-    var c=2
-    return b+c;
+    var c=2;
+    var data = new Date();
+    return b+c+data;
 }
 
+
+var s2 = function (){
+    var b=1;
+    var c=3;
+    var data = new Date();
+    return b+c+data;
+}
+
+
 console.log(s())
+console.log(s2())
